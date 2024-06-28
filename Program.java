c2c727512611c94ba31924d5d5a6963b130ece01
Dhiraj Yadav <yadavdhiraj467@gmail.com>
2024-06-28 12:34:48 +0530

    java file

---
+++ Program.java
@@ -0,0 +1,9 @@
+class Program{
+    public static void main(String[] args) {
+        double num1 = 10.2d;
+        int num2 = (int)num1;
+        System.out.println("Num2 : " +num2);
+    }
+}
+   
+  
